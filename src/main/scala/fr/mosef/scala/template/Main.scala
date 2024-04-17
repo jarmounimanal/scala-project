869d2f9258910f22921c95807852af4b7ce32438
Jarmouni Manal <jarmounimanal@Manal-2.local>
2024-04-17 11:58:01 +0200

    test1

--- src/main/scala/fr/mosef/scala/template/Main.scala
+++ src/main/scala/fr/mosef/scala/template/Main.scala
@@ -20,6 +20,8 @@ object Main extends App with Job {
   } catch {
     case e: java.lang.ArrayIndexOutOfBoundsException => "local[1]"
   }
+
+  //Upload_data
   val SRC_PATH: String = try {
     cliArgs(1)
   } catch {
@@ -27,6 +29,7 @@ object Main extends App with Job {
       "./src/main/resources/drivers.csv"
     }
   }
+
   val SRC_PATH_PARQUET: String = try {
     cliArgs(1)
   } catch {
@@ -35,6 +38,7 @@ object Main extends App with Job {
     }
   }
 
+  // Path des destinations
   val DST_PATH: String = try {
     cliArgs(2)
   } catch {
@@ -42,6 +46,7 @@ object Main extends App with Job {
       "./default/output-writer"
     }
   }
+
   val DST_PATH_PARQUET: String = try {
     cliArgs(2)
   } catch {
@@ -49,6 +54,7 @@ object Main extends App with Job {
       "./default/output-writer-parquet"
     }
   }
+
   val conf = new SparkConf()
   conf.set("spark.driver.memory", "64M")
   conf.set("spark.testing.memory", "471859200")
@@ -66,43 +72,50 @@ object Main extends App with Job {
     .hadoopConfiguration
     .setClass("fs.file.impl",  classOf[BareLocalFileSystem], classOf[FileSystem])
 
+//Initialisation
 
   val reader: Reader = new ReaderImpl(sparkSession)
   val processor: Processor = new ProcessorImpl()
   val writer: Writer = new Writer()
 
+// Attribue le chemin du fichier source pour les données CSV
   val src_path = SRC_PATH
+  // Attribue le chemin du fichier source pour les données parquet
   val src_path_parquet = SRC_PATH_PARQUET
 
+  // Attribue le chemin du fichier destination pour les données CSV
   val dst_path = DST_PATH
+  // Attribue le chemin du fichier destination pour les données parquet
   val dst_path_parquet = DST_PATH_PARQUET
 
-
+//fonction reader
   val inputDF = reader.read(src_path)
   inputDF.show(60)
-
   val inputDFparquet = reader.readParquet(src_path_parquet)
   inputDFparquet.show(60)
 
+  //fonction processor
   val processedDF: DataFrame = processor.process(inputDF)
+  val processedDF_parquet = processor.countRows(inputDFparquet)
+
+//write
   writer.write(processedDF, "overwrite", dst_path)
+  writer.writeParquet(processedDF_parquet, "overwrite", dst_path_parquet)
 
 
 
+//Hive
 
-  val table = "file"
+  val table = "table"
   val tableLocation = "./src/main/ressources"
   processedDF.write
     .mode(SaveMode.Overwrite)
     .option("path", tableLocation)
     .saveAsTable(table)
 
-  val processedDF_parquet = processor.countRows(inputDFparquet)
 
 
-  writer.write(processedDF, "overwrite", dst_path)
-  writer.writeParquet(processedDF_parquet, "overwrite", dst_path_parquet)
-  val hiveTableName = "file"
+  val hiveTableName = "table"
   val hiveTableLocation = "./src/main/ressources"
   val hiveTableDF = reader.readTable(hiveTableName, hiveTableLocation)
   hiveTableDF.show(60)
@@ -110,6 +123,7 @@ object Main extends App with Job {
   val columnName = "sum(value)"
   val processedDF_hive = processor.sum(hiveTableDF, columnName)
 
+//ecriture de la table
   val tableNametoload = "table_loaded"
   val tablePath = "./default/output-writer-hive"
   writer.writeTable(processedDF_hive, tableNametoload, tablePath = tablePath)
